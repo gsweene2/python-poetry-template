a5d68e515777d1d9cfab57c0d57074b2562e7f0c
gsweene2 <gsweene2@u.rochester.edu>
2022-03-19 23:05:39 -0400

    Test S3 Util (#1)
    * Test S3 Util
    
    * Unused Import

---
+++ tests/test_s3_util.py
@@ -0,0 +1,29 @@
+from python_poetry_template import s3_util
+
+from mock import Mock, patch
+
+
+@patch("python_poetry_template.s3_util.boto3")
+def test_list_objects__when_no_objects_in_bucket__should_return_empty_list(mock_boto3):
+    # Arrange
+
+    page_object = Mock()
+    page_object.build_full_result.return_value = {"Contents": []}
+
+    # Iter Mock returns empty array (no objects)
+    iter_mock = Mock()
+    iter_mock.paginate.return_value = page_object
+
+    # Client Mock
+    client_mock = Mock()
+    # Call to Paginator returns Iter Mock
+    client_mock.get_paginator.return_value = iter_mock
+
+    # Call to client returns Client Mock
+    mock_boto3.client.return_value = client_mock
+
+    # Act
+    result = s3_util.list_objects("my-bucket")
+
+    # Assert
+    assert [] == result
