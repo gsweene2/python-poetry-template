481b95b754c405810346a3410c45bbbfbc083096
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2022-08-27 15:01:01 -0400

    Integration Test

--- python_poetry_template/main.py
+++ python_poetry_template/main.py
@@ -1,4 +1,5 @@
 # Use . when importing module in flat structure
+import boto3
 import logging
 
 from .s3_util import list_objects
@@ -6,7 +7,7 @@ from .s3_util_class import S3Util
 
 
 def get_specific_bucket_objects():
-    bucket_name = "garretts-sample-bucket-he-doesnt-own"
+    bucket_name = "www.pubgoer.me"
     objects = list_objects(bucket_name)
     # ToDo: Logging doesn't print to stdout with poetry
     logging.info(f"Found {len(objects)} objects in {bucket_name}")
@@ -15,10 +16,15 @@ def get_specific_bucket_objects():
 
 
 def get_specific_bucket_objects_using_util_class():
-    bucket_name = "garretts-sample-bucket-he-doesnt-own"
-    s3_util = S3Util()
+    bucket_name = "www.pubgoer.me"
+    s3_util = boto3.client("s3")
+    s3_util = S3Util(s3_util)
     objects = s3_util.list_objects(bucket_name)
     # ToDo: Logging doesn't print to stdout with poetry
     logging.info(f"Found {len(objects)} objects in {bucket_name}")
     print(f"Found {len(objects)} objects in {bucket_name}")
     return objects
+
+
+objects = get_specific_bucket_objects_using_util_class()
+print(f"found {len(objects)} objects in the bucket")
