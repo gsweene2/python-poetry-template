7844050a296b73c12608b50dd6ac7523f81ad33e
Garrett <garrett.d.sweeney@gmail.com>
2022-08-26 22:59:16 -0400

    Use DI (#4)

--- python_poetry_template/s3_util_class.py
+++ python_poetry_template/s3_util_class.py
@@ -1,9 +1,6 @@
-import boto3
-
-
 class S3Util:
-    def __init__(self):
-        self.s3_client = boto3.client("s3")
+    def __init__(self, s3_client):
+        self.s3_client = s3_client
 
     def list_objects(self, bucket_name, jmes_exp=None):
         paginator = self.s3_client.get_paginator("list_objects")
--- tests/test_s3_util_class.py
+++ tests/test_s3_util_class.py
@@ -1,56 +1,35 @@
 from python_poetry_template.s3_util_class import S3Util
 
-from mock import Mock, patch
+from mock import MagicMock
 
 
-@patch("python_poetry_template.s3_util_class.boto3")
-def test_list_objects__when_no_objects_in_bucket__should_return_empty_list(mock_boto3):
+def test_list_objects__when_no_objects_in_bucket__should_return_empty_list():
     # Arrange
-
-    page_object = Mock()
-    page_object.build_full_result.return_value = {"Contents": []}
-
-    # Iter Mock returns empty array (no objects)
-    iter_mock = Mock()
-    iter_mock.paginate.return_value = page_object
-
-    # Client Mock
-    client_mock = Mock()
-    # Call to Paginator returns Iter Mock
-    client_mock.get_paginator.return_value = iter_mock
-
-    # Call to client returns Client Mock
-    mock_boto3.client.return_value = client_mock
+    # Mock the s3_client with a return value on chained method calls
+    mock_s3_client = MagicMock()
+    mock_s3_client.get_paginator().paginate().build_full_result.return_value = {"Contents": []}
+    # This also works:
+    # mock_s3_client.get_paginator.return_value.paginate.return_value.build_full_result.return_value = {"Contents": []}
 
     # Act
-    s3_util = S3Util()
+    s3_util = S3Util(mock_s3_client)
     result = s3_util.list_objects("my-bucket")
+    # print(f'calls: {mock_s3_client.mock_calls}')
 
     # Assert
     assert [] == result
 
 
-@patch("python_poetry_template.s3_util_class.boto3")
-def test_list_objects__when_multi_obj_in_bucket__should_return_multi_obj(mock_boto3):
+def test_list_objects__when_multi_obj_in_bucket__should_return_multi_obj():
     # Arrange
-
-    page_object = Mock()
-    page_object.build_full_result.return_value = {"Contents": [{"key": "some_key_value"}, {"key2": "some_key_value2"}]}
-
-    # Iter Mock returns empty array (no objects)
-    iter_mock = Mock()
-    iter_mock.paginate.return_value = page_object
-
-    # Client Mock
-    client_mock = Mock()
-    # Call to Paginator returns Iter Mock
-    client_mock.get_paginator.return_value = iter_mock
-
-    # Call to client returns Client Mock
-    mock_boto3.client.return_value = client_mock
+    # Mock the s3_client with a return value on chained method calls
+    mock_s3_client = MagicMock()
+    mock_s3_client.get_paginator().paginate().build_full_result.return_value = {"Contents": [{"key": "some_key_value"}, {"key2": "some_key_value2"}]}
+    # This also works:
+    # mock_s3_client.get_paginator.return_value.paginate.return_value.build_full_result.return_value = {"Contents": []}
 
     # Act
-    s3_util = S3Util()
+    s3_util = S3Util(mock_s3_client)
     result = s3_util.list_objects("my-bucket")
 
     # Assert
